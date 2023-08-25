585c71772240f9a0dd6c600d502a6c98233d84da
Cesar_Garcia <111899276+Porfesionista@users.noreply.github.com>
2023-08-25 10:31:14 -0700

    Add files via upload

---
+++ project/scrip.js
@@ -0,0 +1,7 @@
+setTimeout(function() {
+    document.getElementById("invisible").classList.remove("Intro_display");
+}, 3000);
+
+setTimeout(function() {
+    document.getElementById("hiddenText").style.display = "block";
+}, 3000); 
